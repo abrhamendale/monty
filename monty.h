a6df041a80d8bab15d6f3baf9a45927f15357c45
Abrham Endale <tmailae2020@gmail.com>
2022-10-06 06:19:08 -0700

    Fixed getline compile error.

--- monty.h
+++ monty.h
@@ -1,7 +1,7 @@
 #ifndef MONTY_H
 #define MONTY_H
-#define  _GNU_SOURCE
-/*#define  _POSIX_C_SOURCE 200809L*/
+/*#define  _GNU_SOURCE*/
+#define  _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
